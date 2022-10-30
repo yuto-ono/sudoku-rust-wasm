8b098836a48fb69c42c48be642b7ed0a62d577d2
Yuto Ono <stellarwindmaster@gmail.com>
2022-10-30 18:46:32 +0900

    Title

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,15 @@
-<main class="main" />
+<script lang="ts">
+  import Board from "./Board.svelte"
+  import Title from "./Title.svelte"
+</script>
+
+<main class="main">
+  <Title>数独を一瞬で解いてしまうプログラム</Title>
+  <Board />
+</main>
 
 <style lang="scss">
   .main {
-    padding-top: 30px;
-    padding-bottom: 80px;
+    padding: 30px 10px 80px;
   }
 </style>
---
+++ src/Title.svelte
@@ -0,0 +1,10 @@
+<h1 class="title"><slot /></h1>
+
+<style lang="scss">
+  .title {
+    margin-bottom: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+  }
+</style>
