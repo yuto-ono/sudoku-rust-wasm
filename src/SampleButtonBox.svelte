883a92601c7bf54c14508272c8a39a96d7f4685c
Yuto Ono <stellarwindmaster@gmail.com>
2022-11-03 12:51:21 +0900

    Update SampleButtonBox.svelte

--- src/SampleButtonBox.svelte
+++ src/SampleButtonBox.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
   import Button from "./Button.svelte"
   import { sample1, sample2 } from "./samples"
-  import { cells } from "./stores"
+  import { cells, solved } from "./stores"
+
+  const setSample = (sample: number[]) => {
+    cells.setArray(sample)
+    $solved = false
+  }
 </script>
 
 <div class="btn-box">
   <div class="btn-wrapper">
-    <Button variant="primary" on:click={() => cells.setArray(sample1)}>
+    <Button variant="primary" on:click={() => setSample(sample1)}>
       サンプル1
     </Button>
   </div>
   <div class="btn-wrapper">
-    <Button variant="primary" on:click={() => cells.setArray(sample2)}>
+    <Button variant="primary" on:click={() => setSample(sample2)}>
       サンプル2
     </Button>
   </div>
